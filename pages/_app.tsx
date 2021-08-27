edf9d4681b8f70a9001fc3fd1d01d782da91d509
taku1115 <59477643+taku1115@users.noreply.github.com>
2021-08-27 18:01:23 +0900

    setting eslint & prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // document.body.style.color === "red";
+  const [state] = useState();
+
+  useEffect(() => {
+    if (state) return;
+  }, [state]);
+
+  return <Component {...pageProps} />;
 }
-export default MyApp
+export default MyApp;
