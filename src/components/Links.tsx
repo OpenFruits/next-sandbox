20d4b5b4354cd72dd1bae274d94bda18e88a1990
taku1115 <59477643+taku1115@users.noreply.github.com>
2021-08-30 13:26:15 +0900

    ♻️ LInks

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -2,71 +2,82 @@ import { VFC } from "react";
 import { sp } from "../../styles/media";
 import styled from "styled-components";
 
+const ITEMS = [
+  {
+    href: "https://nextjs.org/docs",
+    title: "Documentation →",
+    description: "Find in-depth information about Next.js features and API.",
+  },
+  {
+    href: "https://nextjs.org/learn",
+    title: "Learn →",
+    description: "Learn about Next.js in an interactive course with quizzes!",
+  },
+  {
+    href: "https://github.com/vercel/next.js/tree/master/examples",
+    title: "Examples →",
+    description: "Discover and deploy boilerplate example Next.js projects.",
+  },
+  {
+    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Deploy →",
+    description:
+      "Instantly deploy your Next.js site to a public URL with Vercel.",
+  },
+];
+
+const Grid = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  max-width: 800px;
+  margin-top: 3rem;
+  ${sp`
+      width: 20px;
+      height: 20px;
+  `}
+`;
+
+const Card = styled.a`
+  margin: 1rem;
+  padding: 1.5rem;
+  text-align: left;
+  color: inherit;
+  text-decoration: none;
+  border: 1px solid #eaeaea;
+  border-radius: 10px;
+  transition: color 0.15s ease, border-color 0.15s ease;
+  width: 45%;
+
+  &:hover,
+  &:focus,
+  &:active {
+    color: #0070f3;
+    border-color: #0070f3;
+  }
+
+  h2 {
+    margin: 0 0 1rem 0;
+    font-size: 1.5rem;
+  }
+
+  p {
+    margin: 0;
+    font-size: 1.25rem;
+    line-height: 1.5;
+  }
+`;
+
 export const Links: VFC = () => {
-  const Grid = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-wrap: wrap;
-    max-width: 800px;
-    margin-top: 3rem;
-    ${sp`
-        width: 20px;
-        height: 20px;
-    `}
-  `;
-
-  const Card = styled.a`
-    margin: 1rem;
-    padding: 1.5rem;
-    text-align: left;
-    color: inherit;
-    text-decoration: none;
-    border: 1px solid #eaeaea;
-    border-radius: 10px;
-    transition: color 0.15s ease, border-color 0.15s ease;
-    width: 45%;
-
-    &:hover,
-    &:focus,
-    &:active {
-      color: #0070f3;
-      border-color: #0070f3;
-    }
-
-    h2 {
-      margin: 0 0 1rem 0;
-      font-size: 1.5rem;
-    }
-
-    p {
-      margin: 0;
-      font-size: 1.25rem;
-      line-height: 1.5;
-    }
-  `;
-
   return (
     <Grid>
-      <Card href="https://nextjs.org/docs">
-        <h2>Documentation &rarr;</h2>
-        <p>Find in-depth information about Next.js features and API.</p>
-      </Card>
-
-      <Card href="https://nextjs.org/learn">
-        <h2>Learn &rarr;</h2>
-        <p>Learn about Next.js in an interactive course with quizzes!</p>
-      </Card>
-
-      <Card href="https://github.com/vercel/next.js/tree/master/examples">
-        <h2>Examples &rarr;</h2>
-        <p>Discover and deploy boilerplate example Next.js projects.</p>
-      </Card>
-
-      <Card href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
-        <h2>Deploy &rarr;</h2>
-        <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-      </Card>
+      {ITEMS.map((item) => (
+        <Card key={item.href} href={item.href}>
+          <h2>{item.title}</h2>
+          <p>{item.description}</p>
+        </Card>
+      ))}
     </Grid>
   );
 };
