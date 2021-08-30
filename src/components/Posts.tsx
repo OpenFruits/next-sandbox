8120732a2401b31baf73f1b2eb21f0540876a99c
taku1115 <59477643+taku1115@users.noreply.github.com>
2021-08-30 21:19:07 +0900

    🎨 Replace to swr

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,4 +1,5 @@
-import { VFC, useCallback, useEffect, useState } from "react";
+import { VFC } from "react";
+import { usePosts } from "src/hooks/usePosts";
 
 type Post = {
   id: number;
@@ -8,55 +9,18 @@ type Post = {
 };
 
 export const Posts: VFC = () => {
-  const [state, setState] = useState<{
-    data: Post[];
-    loading: boolean;
-    error: Error | null;
-  }>({
-    data: [],
-    loading: true,
-    error: null,
-  });
+  const { data, error, isLoading, isEmpty } = usePosts();
 
-  const getPosts = useCallback(async () => {
-    try {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      if (!res.ok) {
-        throw new Error("エラーが発生したため、データの取得に失敗しました。");
-      }
-      const json = await res.json();
-      setState((prevState) => {
-        return {
-          ...prevState,
-          data: json,
-          loading: false,
-        };
-      });
-    } catch (error) {
-      setState((prevState) => {
-        return {
-          ...prevState,
-          loading: false,
-          error,
-        };
-      });
-    }
-  }, []);
+  if (isLoading) return <div>ローディング中</div>;
 
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
+  if (error) return <div>{error.message}</div>;
 
-  if (state.loading) return <div>ローディング中</div>;
-
-  if (state.error) return <div>{state.error?.message}</div>;
-
-  if (state.data.length === 0) return <div>データが空です</div>;
+  if (isEmpty) return <div>データが空です</div>;
 
   return (
     <div>
       <ol>
-        {state.data.map((post) => (
+        {data.map((post: Post) => (
           <li key={post.id}>{post.title}</li>
         ))}
       </ol>
