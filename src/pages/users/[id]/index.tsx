ae5a1701df50c7d22a61536d29c2d1c50b94bc70
taku1115 <59477643+taku1115@users.noreply.github.com>
2021-09-06 10:02:32 +0900

    ✨ SSR with SWR

--- src/pages/users/[id]/index.tsx
+++ src/pages/users/[id]/index.tsx
@@ -1,18 +1,38 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import { Header } from "src/components/Header";
 import { User } from "src/components/User";
 import styled from "styled-components";
+import { SWRConfig } from "swr";
 
 const Container = styled.div`
   min-height: 100vh;
   padding: 0 0.5rem;
 `;
 
-const UsersId: NextPage = () => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { id } = ctx.query;
+  const API_URL = `https://jsonplaceholder.typicode.com/users/${id}`;
+  const user = await fetch(API_URL);
+  const userData = await user.json();
+
+  return {
+    props: {
+      fallback: {
+        [API_URL]: userData,
+      },
+    },
+  };
+};
+
+const UsersId: NextPage = (props: any) => {
+  const { fallback } = props;
+
   return (
     <Container>
       <Header />
-      <User />
+      <SWRConfig value={{ fallback }}>
+        <User />
+      </SWRConfig>
     </Container>
   );
 };
