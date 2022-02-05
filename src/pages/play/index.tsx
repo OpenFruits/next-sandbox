b9aa8801ea999502192394dd38417bd84398b063
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-02-05 16:44:17 +0900

    💄

--- src/pages/play/index.tsx
+++ src/pages/play/index.tsx
@@ -1,13 +1,12 @@
 import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { Header } from "src/components/Header";
-import styled from "styled-components";
-import { Headline } from "src/components/separate/Headline";
 import React, { VFC } from "react";
 import { API_URL } from "src/utils/const";
 import useSWR, { SWRConfig } from "swr";
 import { Post, Props } from "src/utils/types";
 import { useCounter } from "src/hooks/play/useCounter";
+import { Container } from "src/styles/Container";
 
 export const getStaticProps: GetStaticProps<Props<Post[]>> = async () => {
   const res = await fetch(`${API_URL}/posts`);
@@ -24,11 +23,11 @@ export const getStaticProps: GetStaticProps<Props<Post[]>> = async () => {
 const ChildComponent: VFC<{ isSG: boolean }> = ({ isSG }) => {
   const { data, error } = useSWR(`${API_URL}/posts`);
 
-  if (isSG) return <h2>SGでPostsの数を取得→「{data.length}」</h2>;
+  if (isSG) return <h3>SGでPostsの数を取得→「{data.length}」</h3>;
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
 
-  return <h2>CSRでPostsの数を取得→「{data.length}」</h2>;
+  return <h3>CSRでPostsの数を取得→「{data.length}」</h3>;
 };
 
 const Play: NextPage<Props<Post[]>> = (props) => {
@@ -43,15 +42,13 @@ const Play: NextPage<Props<Post[]>> = (props) => {
 
       <Header />
 
-      <Headline page="play" />
-
       <ChildComponent isSG={false} />
       <SWRConfig value={{ fallback }}>
         <ChildComponent isSG />
       </SWRConfig>
 
-      {counter.isShow && <h2>{`Count：${counter.count}`}</h2>}
-      {counter.isShow && <h2>{`DoubleCount：${counter.doubleCount}`}</h2>}
+      {counter.isShow && <h3>{`Count：${counter.count}`}</h3>}
+      {counter.isShow && <h3>{`DoubleCount：${counter.doubleCount}`}</h3>}
       {counter.isShow && (
         <button onClick={counter.handleClick}>Count Up</button>
       )}
@@ -63,12 +60,3 @@ const Play: NextPage<Props<Post[]>> = (props) => {
 };
 
 export default Play;
-
-const Container = styled.div`
-  min-height: 100vh;
-  padding: 0 0.5rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
